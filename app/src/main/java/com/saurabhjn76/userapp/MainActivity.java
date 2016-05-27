5d73bd3f5b6ec06b8f2c93e0db1bbf9e9b1fa1c4
saurabhjn76 <saurabhjn76@gmail.com>
2016-05-27 20:09:10 +0530

    Added data to the data node at firebase

--- app/src/main/java/com/saurabhjn76/userapp/MainActivity.java
+++ app/src/main/java/com/saurabhjn76/userapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,11 +13,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Firebase.setAndroidContext(this);
+        writeData();
 
     }
-    void wrtieData()
+    void writeData()
     {
         Firebase ref = new Firebase("https://salon-app-dad97.firebaseio.com/data");
+        Salon[] s= new Salon[11];
+        s[0]=new Salon("Affinity Salon","1st Floor, Global Foyer Mall","Golf Course Road","Unisex",4.2,true,true,4);
+        s[1]=new Salon("Neu Salonz","4th Floor","South Point Mall","Unisex",4.1,true,true,8);
+        s[2]=new Salon("Bella Madonna","123 1st Floor","South Point Mall","Unisex",4.2,true,true,0.6);
+        s[3]=new Salon("Levo Saplon","Ibis Hotel","Golf Course Road","Unisex",5,false,true,0.3);
+        s[4]=new Salon("")
+
+        ref.push().setValue(s[0]);
+        ref.push().setValue(s[1]);
+        ref.push().setValue(s[2]);
+        ref.push().setValue(s[3]);
+       /* ref.setValue(s[0], new Firebase.CompletionListener() {
+            @Override
+            public void onComplete(FirebaseError firebaseError, Firebase firebase) {
+                if (firebaseError != null) {
+                    System.out.println("Data could not be saved. " + firebaseError.getMessage());
+                } else {
+                    System.out.println("Data saved successfully.");
+                }
+            }
+        });*/
+
+
+
 
     }
 }
--- app/src/main/java/com/saurabhjn76/userapp/Salon.java
+++ app/src/main/java/com/saurabhjn76/userapp/Salon.java
@@ -4,17 +4,53 @@ package com.saurabhjn76.userapp;
  * Created by saurabh on 27/5/16.
  */
 public class Salon {
-    private int birthYear;
-    private String fullName;
+    private String salonName;
+    private String addressLine1;
+    private String type;
+    private String addressLine2;
+    private double rating;
+    private boolean tv;
+    private boolean ac;
+    private double distance;
+
     public Salon() {}
-    public Salon(String fullName, int birthYear) {
-        this.fullName = fullName;
-        this.birthYear = birthYear;
+
+    public Salon(String salonName, String addressLine1,String addressLine2, String type ,double rating, boolean tv,boolean ac,double distance) {
+        this.salonName = salonName;
+        this.addressLine1 = addressLine1;
+        this.addressLine2=addressLine2;
+        this.type=type;
+        this.rating=rating;
+        this.tv=tv;
+        this.ac=ac;
+        this.distance=distance;
     }
-    public long getBirthYear() {
-        return birthYear;
+
+    public String getSalonName() {
+        return salonName;
     }
-    public String getFullName() {
-        return fullName;
+    public String getAddressLine1() {
+        return addressLine1;
     }
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+    public String getType(){
+        return type;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+    public boolean getTv(){
+        return tv;
+    }
+    public boolean getAc(){
+        return ac;
+    }
+
 }
